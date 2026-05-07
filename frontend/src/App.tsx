10722459bc0736685bb75d401b6d3ed6ee75d304
Josh <Caoshutong01@gmail.com>
2026-05-08 07:38:27 +1200

    feat(frontend): wire /forgot-password and /reset-password routes into App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { HomePage } from './pages/HomePage'
 import { MePage } from './pages/MePage'
 import { RegisterPage } from './pages/RegisterPage'
 import { LoginPage } from './pages/LoginPage'
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage'
+import { ResetPasswordPage } from './pages/ResetPasswordPage'
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="/me" element={<ProtectedRoute><MePage /></ProtectedRoute>} />
         </Routes>
       </AuthProvider>
