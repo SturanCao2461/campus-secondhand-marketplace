214907c55a33c982a71319b4c0e29369dcc736b5
Josh <Caoshutong01@gmail.com>
2026-05-08 07:07:03 +1200

    feat(frontend): add LoginPage with ?next= redirect (same-origin validated)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar'
 import { HomePage } from './pages/HomePage'
 import { MePage } from './pages/MePage'
 import { RegisterPage } from './pages/RegisterPage'
+import { LoginPage } from './pages/LoginPage'
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/me" element={<ProtectedRoute><MePage /></ProtectedRoute>} />
         </Routes>
       </AuthProvider>
